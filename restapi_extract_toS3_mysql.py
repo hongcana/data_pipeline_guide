34ded96c1b5db6a5bb7adaa7c0a8be0ffbaa3c03
hongcana <hongcana@kakao.com>
2023-05-13 17:19:38 +0900

    REST API에서 데이터 추출하기 예제

---
+++ restapi_extract_toS3_mysql.py
@@ -0,0 +1,41 @@
+import requests as rs
+import json
+import csv
+import configparser
+import boto3
+from datetime import datetime
+
+api_response = rs.get("http://api.open-notify.org/iss-now.json")
+
+# json 형식으로 출력해보는 코드
+# print(api_response.content)
+
+# json 처리
+response_json = json.loads(api_response.content)
+datas = []
+
+location = response_json["iss_position"]
+datas.append(location["latitude"])
+datas.append(location["longitude"])
+datas.append(
+    datetime.fromtimestamp(response_json["timestamp"]).strftime("%Y-%m-%d %H:%M")
+)
+
+print(datas)
+export_file = "export_file.csv"
+
+with open(export_file, "w", newline="") as fp:
+    csvw = csv.writer(fp)
+    csvw.writerows([datas])  # 2차원 리스트로 처리
+fp.close()
+
+
+# load aws_boto_credentials value
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+access_key = parser.get("aws_boto_credentials", "access_key")
+secret_key = parser.get("aws_boto_credentials", "secret_key")
+bucket_name = parser.get("aws_boto_credentials", "bucket_name")
+
+s3 = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+s3.upload_file(export_file, bucket_name, export_file)
